736504d86ec2d231b46a6efd0ee86c87b051c1fa
Loai Alwan <118984533+Meliods23@users.noreply.github.com>
2025-01-31 10:36:00 +0100

    Implemented class "TodoItem" with all required methods

--- src/main/java/se/lexicon/ToDo/TodoItem.java
+++ src/main/java/se/lexicon/ToDo/TodoItem.java
@@ -27,4 +27,83 @@ public class TodoItem {
 
         this.done = done;
     }
+
+    // **********************************************
+    // Getter methods
+    // **********************************************
+
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTaskDescription() {
+        return description;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public String getSummary() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Id : ").append(Id + ", ")
+                .append("Title : ").append(title + ", ")
+                .append("DeadLine : ").append(deadLine + "\n")
+                .append("Task Description --> \n").append(description);
+
+        return sb.toString();
+    }
+
+    // **********************************************
+    // Setter methods
+    // **********************************************
+
+
+    public void setTitle(String title) {
+        if (title != null && !title.isEmpty()) {
+            this.title = title;
+        }
+    }
+
+    public void setTaskDescription(String description) {
+        if (description != null) {
+            this.description = description;
+        }
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        if (deadLine != null) {
+            this.deadLine = deadLine;
+        }
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+
+    public void setCreator(Person creator) {
+        if (creator != null) {
+            this.creator = creator;
+        }
+    }
+
+    // **********************************************
+    // Other methods
+    // **********************************************
+
+    public boolean isOverdue()
+    {
+        return LocalDate.now().isAfter(deadLine);
+    }
 }
