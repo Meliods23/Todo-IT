644ef9d8ae2219eef9b167a79c7e9e8ae1099f79
Loai Alwan <118984533+Meliods23@users.noreply.github.com>
2025-01-31 10:12:29 +0100

    Initialized "Person" and "TodoItem" classes
    Both classes have their variables added in addition to a basic constructor with null checks.

--- src/main/java/se/lexicon/ToDo/Person.java
+++ src/main/java/se/lexicon/ToDo/Person.java
@@ -2,5 +2,21 @@ package se.lexicon.ToDo;
 
 public class Person
 {
+    private int Id;
+    private String firstName;
+    private String lastName;
+    private String email;
 
+    public Person(String firstName, String lastName, String email)
+    {
+        if (firstName != null) {
+            this.firstName = firstName;
+        }
+        if (lastName != null) {
+            this.lastName = lastName;
+        }
+        if (email != null) {
+            this.email = email;
+        }
+    }
 }
--- src/main/java/se/lexicon/ToDo/TodoItem.java
+++ src/main/java/se/lexicon/ToDo/TodoItem.java
@@ -1,5 +1,30 @@
 package se.lexicon.ToDo;
 
-public class TodoItem {
+import java.time.LocalDate;
 
+public class TodoItem {
+    private int Id;
+    private String title;
+    private String description;
+    private LocalDate deadLine;
+    private boolean done;
+    private Person creator;
+
+    public TodoItem(String title, String description, LocalDate deadLine, boolean done, Person creator)
+    {
+        if (title != null && !title.isEmpty()) {
+            this.title = title;
+        }
+        if (description != null) {
+            this.description = description;
+        }
+        if (deadLine != null) {
+            this.deadLine = deadLine;
+        }
+        if (creator != null) {
+            this.creator = creator;
+        }
+
+        this.done = done;
+    }
 }
