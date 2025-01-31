5623af6a333b36d7f56e3cb665a2e12a5cbea1e4
Loai Alwan <118984533+Meliods23@users.noreply.github.com>
2025-01-31 10:46:33 +0100

    "TodoItemTask" class initialized and implemented fully.

---
+++ src/main/java/se/lexicon/ToDo/TodoItemTask.java
@@ -0,0 +1,60 @@
+package se.lexicon.ToDo;
+
+public class TodoItemTask {
+    private int id;
+    private boolean assigned;
+    private TodoItem todoItem;
+    private Person assignee;
+
+
+    // **********************************************
+    // Getter methods
+    // **********************************************
+
+    public int getId() {
+        return id;
+    }
+
+    public TodoItem getTodoItem() {
+        return todoItem;
+    }
+
+    public Person getAssignee() {
+        return assignee;
+    }
+
+    public String getSummary()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Task is --> ").append(todoItem.getSummary())
+                .append("Person assigned the task is --> ").append(assignee.getSummary());
+
+        return sb.toString();
+    }
+    // **********************************************
+    // Setter methods
+    // **********************************************
+
+
+    public void setAssigned(boolean assigned) {
+        this.assigned = assigned;
+    }
+
+    public void setTodoItem(TodoItem todoItem) {
+        if (todoItem != null) {
+            this.todoItem = todoItem;
+        }
+    }
+
+    public void setAssignee(Person assignee) {
+        if (assignee != null) {
+            this.assignee = assignee;
+            this.assigned = true;
+        }
+    }
+
+    // **********************************************
+    // Other methods
+    // **********************************************
+}
